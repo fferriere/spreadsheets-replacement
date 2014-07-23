dd0dfdf0b6a4853fbd3d84a51513e05b7c830f3f
FERRIERE Florian <ferriere.florian@gmail.com>
2014-07-23 11:52:02 +0200

    Add an inherit class of Zend\Stdlib\PriorityList for add a method for set FIFO sort method

---
+++ src/SpreadsheetsReplacement/PriorityList.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace SpreadsheetsReplacement;
+
+use Zend\Stdlib\PriorityList as ZendPriorityList;
+
+/**
+ * Description of PriorityList
+ *
+ * @author florian
+ */
+class PriorityList extends ZendPriorityList {
+
+    /**
+     * Change the order of the list for FIFO.
+     */
+    public function setFIFO() {
+        return !$this->isLIFO(false);
+    }
+
+    /**
+     * Change the order of the list for LIFO.
+     */
+    public function setLIFO() {
+        return $this->isLIFO(true);
+    }
+
+}
