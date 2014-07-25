1e393da42e2dd82755b9010fd251e66bd17c6771
FERRIERE Florian <ferriere.florian@gmail.com>
2014-07-25 14:14:33 +0200

    Add an action for replace a complete value by another

---
+++ src/SpreadsheetsReplacement/Action/FullReplace.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace SpreadsheetsReplacement\Action;
+
+/**
+ * Description of FullReplace
+ *
+ * @author florian
+ */
+class FullReplace extends Action {
+
+    protected $search;
+
+    protected $replace;
+
+    public function __construct($search = null, $replacement = null) {
+        $this->search = $search;
+        $this->replace = $replacement;
+    }
+
+    /**
+     * Returns the search element.
+     * @return string
+     */
+    public function getSearch() {
+        return $this->search;
+    }
+
+    /**
+     * Modify the search element.
+     * @param string $search
+     */
+    public function setSearch($search) {
+        $this->search = $search;
+    }
+
+    /**
+     * Returns the replace element.
+     * @return string
+     */
+    public function getReplace() {
+        return $this->replace;
+    }
+
+    /**
+     * Modify the replace element
+     * @param string $replace
+     */
+    public function setReplace($replace) {
+        $this->replace = $replace;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function replace($value) {
+        if ($value == $this->search) {
+            return $this->replace;
+        }
+        return $value;
+    }
+
+}
---
+++ test/src/SpreadsheetsReplacement/Action/FullReplace.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace SpreadsheetsReplacement\Action\tests\units;
+
+// / tests / src / SpreadsheetsReplacement / Action / FullReplace
+require_once dirname(dirname(dirname(dirname(__DIR__)))).'/vendor/autoload.php';
+
+use atoum;
+
+/**
+ * Test class for FullReplace.
+ *
+ * @author florian
+ */
+class FullReplace extends atoum {
+
+    public function testReplace() {
+        $action = new \SpreadsheetsReplacement\Action\FullReplace('VE', 'VTE');
+        $value = 'VE';
+        $this->variable('VTE')
+                ->isEqualTo($action->replace($value));
+    }
+
+    public function testNoReplace() {
+        $action = new \SpreadsheetsReplacement\Action\FullReplace('VE', 'VTE');
+        $value = 'AC';
+        $this->variable($value)
+                ->isEqualTo($action->replace($value));
+    }
+
+}
